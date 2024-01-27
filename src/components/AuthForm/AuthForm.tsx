5325fa525e6f7cce0b33feb8e4d0b60bf9b58ec7
Jonjo7Dia <jonosphgames@gmail.com>
2024-01-27 20:55:51 +0100

    add navigation authform to homepage

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,8 +1,23 @@
 import { Box, Button, Input, VStack, Text, Link } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    username: "",
+    confirmPassword: "",
+  });
+  const handleAuth = () => {
+    if (!inputs.email || !inputs.password) {
+      alert("please fill all the fields");
+      return;
+    }
+    navigate("/");
+  };
   return (
     <>
       <Box>
@@ -12,6 +27,7 @@ const AuthForm = () => {
             fontSize={16}
             type={"email"}
             borderColor={"BBC8D4"}
+            onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
           />
           {!isLogin && (
             <Input
@@ -19,12 +35,16 @@ const AuthForm = () => {
               fontSize={16}
               type={"text"}
               borderColor={"BBC8D4"}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           )}
           <Input
             placeholder={"Password"}
             borderColor={"BBC8D4"}
             type={"password"}
+            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
           />
 
           {!isLogin && (
@@ -33,10 +53,13 @@ const AuthForm = () => {
               fontSize={16}
               type={"password"}
               borderColor={"BBC8D4"}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
             />
           )}
 
-          <Button w={"full"} colorScheme={"blue"}>
+          <Button w={"full"} colorScheme={"blue"} onClick={handleAuth}>
             {isLogin ? "Log in" : "Signup"}
           </Button>
           {isLogin && (
