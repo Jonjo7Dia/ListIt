1b66fc97474001777e2cc9df06ae6a6e8205dd95
Jonjo7Dia <jonosphgames@gmail.com>
2024-01-28 19:10:48 +0100

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_APP_FIRESTORE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIRESTORE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, firestore, storage };
