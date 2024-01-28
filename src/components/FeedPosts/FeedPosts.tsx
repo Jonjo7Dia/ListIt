c9a1945b79b48ee7af2f72a7833a3631585a1a3c
Jonjo7Dia <jonosphgames@gmail.com>
2024-01-28 16:32:46 +0100

    Add skeleton

--- src/components/FeedPosts/FeedPosts.tsx
+++ src/components/FeedPosts/FeedPosts.tsx
@@ -1,14 +1,48 @@
-import { Container } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  VStack,
+  SkeletonCircle,
+  Skeleton,
+  Box,
+} from "@chakra-ui/react";
 import FeedPost from "./FeedPost";
+import { useEffect, useState } from "react";
 
 const FeedPosts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
   return (
     <Container width={"100%"} py={4} px={2}>
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      {isLoading &&
+        [0, 1, 2].map((_, idx: number) => (
+          <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+            <Flex gap="2">
+              <SkeletonCircle size="10" />
+              <VStack gap={2} alignItems={"flex-start"}>
+                <Skeleton height="10px" w={"100px"} />
+                <Skeleton height="10px" w={"100px"} />
+              </VStack>
+            </Flex>
+            <Skeleton w={"full"}>
+              <Box h={"300px"}>contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
+      {!isLoading && (
+        <>
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+        </>
+      )}
     </Container>
   );
 };
